3c77913e13cd1c1481218f7d3a139685da51f096
StaubRobin <robin.staub@campus.tu-berlin.de>
2022-12-16 19:11:24 +0100

    Add testbench to vivado project

--- VGA_build_flow.tcl
+++ VGA_build_flow.tcl
@@ -26,12 +26,8 @@ import_ip src/blk_mem_gen_1.xci
 file copy -force assets/large_img.coe $outputDir/$ProjName.srcs/sources_1/ip/blk_mem_gen_1/
 import_ip src/clk_wiz_0.xci
 
-
 add_files -fileset constrs_1 NexysA7_VGA.xdc
-# add_files -fileset sim_1 sim/toplevel_testbench.sv
-# add_files -fileset sim_1 sim/gauss_kernel_tb.vhd
-# add_files -fileset sim_1 sim/linebuffer_tb.vhd
-# add_files -fileset sim_1 sim/kernel_top_tb.vhd
+add_files -fileset sim_1 sim/toplevel_testbench.sv
 # set_property file_type {VHDL 2008} [get_files *_tb.vhd]
 # import_files -force -norecurse
 set_property top vga_sys_top [current_fileset]
@@ -41,16 +37,9 @@ update_compile_order -fileset sources_1
 
 # launch_runs synth_1
 # wait_on_run synth_1
+# puts "Synthesis done!"
 
 # launch_runs impl_1 -to_step write_bitstream
 # wait_on_run impl_1
 # puts "Implementation done!"
 
-# set_param labtools.override_cs_server_version_check 1
-# open_hw_manager
-# connect_hw_server -allow_non_jtag
-# open_hw_target
-# set_property PROGRAM.FILE {$outputDir/MuraxCoremark.runs/impl_1} [get_hw_devices xc7a100t_0]
-# current_hw_device [get_hw_devices xc7a100t_0]
-# refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xc7a100t_0] 0]
-# program_hw_devices [get_hw_devices xc7a100t_0]
